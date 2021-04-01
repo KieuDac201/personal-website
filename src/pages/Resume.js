78b29ed0457c2d9587dd54a422814718044bd52f
KieuDac <kieudac18082001@gmail.com>
2021-04-01 21:10:07 +0700

    add cv

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -7,13 +7,13 @@ const Resume = () => {
             <div className="container">
                 <div className="title">Resume</div>
                 <div className="resume-box">
-                    <div className="resume-title"><span>Education</span><button className="btn">Download CV</button></div>
+                    <div className="resume-title"><span>Education</span><a href="https://i.topcv.vn/kieudaidac?ref=3966510" target="_blank" className="btn">Download CV</a></div>
+                    
                     <div className="resume-content">
                         <div className="resume-info">
-                            <div className="resume-time">2019 - 2022</div>
-                            <h5>HA NOI TRADE & TECH COLLEGE</h5>
-                            <p>Degree Level</p>
-                            <p>Tan Lap, Dan Phuong</p>
+                            <div className="resume-time">2016 - 2019</div>
+                            <h5>THACH THAT HIGHSCHOOL</h5>
+                            <p>Thach That, Ha Noi</p>
                         </div>
                         <p className="resume-desc">
                         I'm a paragraph. Click here to add your own text and edit me. 
@@ -26,18 +26,15 @@ const Resume = () => {
                     </div>
                     <div className="resume-content">
                         <div className="resume-info">
-                            <div className="resume-time">2019 - Present</div>
-                            <h5>JOB POSITION</h5>
-                            <p>Company Name</p>
-                            <p>Company Location</p>
+                            <div className="resume-time">2019 - PRESENT</div>
+                            <h5>HA NOI TRADE & TECH COLLEGE</h5>
+                            <p>Degree Level</p>
+                            <p>Tan Lap, Dan Phuong</p>
                         </div>
                         <p className="resume-desc">
-                        I'm a paragraph. Click here to add your own text and edit me. 
-                        It’s easy. Just click “Edit Text” or double click me to add your 
-                        own content and make changes to the font.
-                        <br />
-                        <br />
-                        I’m a great place for you to tell a story and let your users know a little more about you.
+                        My objective is become profession front end developer. I always expect to have a job about web design.
+                        Especially, i wanna make the best web products for customers with all passionate and complete project on  time.
+                        
                         </p>
                     </div>
                     <div className="resume-content">
