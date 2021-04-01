dc9a3d4b953d8d437984d8028c5fcfdd36f359f1
KieuDac <kieudac18082001@gmail.com>
2021-04-01 15:56:13 +0700

    responsive

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
-import './style/Responsive.css'
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Project from './pages/Project';
 import Resume from './pages/Resume';
+import './style/Responsive.css'
 
 function App() {
   return (
