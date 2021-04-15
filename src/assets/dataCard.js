9ff207566e6dcc4bd578357bdae03c89ac7f684e
KieuDac <kieudac18082001@gmail.com>
2021-04-15 21:21:18 +0700

    add project

--- src/assets/dataCard.js
+++ src/assets/dataCard.js
@@ -2,6 +2,7 @@ import card1 from '../assets/img/d.png'
 import card2 from '../assets/img/image-hero.jpg'
 import card3 from '../assets/img/das.png'
 import card4 from '../assets/img/todo.png'
+import card5 from '../assets/img/film.png'
 
 const dataCard = [
     {
@@ -31,6 +32,13 @@ const dataCard = [
         text: "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.",
         img: card4,
         link: 'https://kieudac201.github.io/todo-app/'
+    },
+    {
+        title: 'Film App',
+        subTitle: 'App',
+        text: "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.",
+        img: card5,
+        link: 'https://kieudac201.github.io/film-app/'
     },
 ]
 export default dataCard;
