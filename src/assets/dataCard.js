c611e44c5167ced8f6fbaa601c4b51779db56bb7
KieuDac <kieudac18082001@gmail.com>
2021-04-21 10:01:17 +0700

    add project

--- src/assets/dataCard.js
+++ src/assets/dataCard.js
@@ -3,8 +3,16 @@ import card2 from '../assets/img/image-hero.jpg'
 import card3 from '../assets/img/das.png'
 import card4 from '../assets/img/todo.png'
 import card5 from '../assets/img/film.png'
+import card6 from '../assets/img/landing-page.png'
 
 const dataCard = [
+    {
+        title: 'Lading Page',
+        subTitle: 'React Js',
+        text: "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.",
+        img: card6,
+        link: 'https://clever-volhard-eb25c2.netlify.app/'
+    },
     {
         title: 'The Weshala',
         subTitle: 'Landing Page',
@@ -20,25 +28,12 @@ const dataCard = [
         link: 'https://kieudac201.github.io/loopstudios-landing-page-main.github.io/'
     },
     {
-        title: 'Portfolio',
-        subTitle: 'Landing Page',
-        text: "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.",
-        img: card3,
-        link: 'https://kieudac201.github.io/kieudac.github.io/'
-    },
-    {
         title: 'Todo App',
         subTitle: 'App',
         text: "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.",
         img: card4,
         link: 'https://kieudac201.github.io/todo-app/'
     },
-    {
-        title: 'Film App',
-        subTitle: 'App',
-        text: "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.",
-        img: card5,
-        link: 'https://kieudac201.github.io/film-app/'
-    },
+    
 ]
 export default dataCard;
